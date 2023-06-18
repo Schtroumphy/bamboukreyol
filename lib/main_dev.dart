ded202252676eedc2732b1aec7da9fe67cb88f86
Thessalène JL <jean-louis.th@hotmail.fr>
2023-06-18 14:02:44 +0200

    config : Finish firebase initialization

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 
 import 'firebase_options_dev.dart';
 import 'flavors.dart';
@@ -6,6 +7,7 @@ import 'main.dart' as runner;
 
 Future<void> main() async {
   F.appFlavor = Flavor.dev;
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await runner.main();
