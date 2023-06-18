09ae641a8b4fa3c78cd32df39d702ceeb85a99d9
Thessalène JL <jean-louis.th@hotmail.fr>
2023-06-18 13:01:19 +0200

    config : Configure firebase for dev env

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options_dev.dart';
 import 'flavors.dart';
 import 'main.dart' as runner;
 
 Future<void> main() async {
   F.appFlavor = Flavor.dev;
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   await runner.main();
 }
