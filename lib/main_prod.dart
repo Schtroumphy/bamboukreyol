0e67681c1fab5839a81c26ccfac2c85979012cd2
Thessalène JL <jean-louis.th@hotmail.fr>
2023-06-18 13:44:40 +0200

    config : Configure firebase for prod env

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options_prod.dart';
 import 'flavors.dart';
 import 'main.dart' as runner;
 
 Future<void> main() async {
   F.appFlavor = Flavor.prod;
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   await runner.main();
 }
